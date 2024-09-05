ef3d71848e6382a03c8e2581e7ea790ab91a1a8d
baeyongwoo <dyddn30612@naver.com>
2024-09-05 22:32:20 +0900

    한글 데이터 대량 넣기 가능

--- IOgit/src/main/java/com/io/task/PermitBoardInsertTask.java
+++ IOgit/src/main/java/com/io/task/PermitBoardInsertTask.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.io.model.TboardDTO;
 import com.io.service.BoardService;
@@ -21,30 +22,31 @@ public class PermitBoardInsertTask {
 	BoardService bs;
 	
 	
-	//정해진 시간에 boardtable에 올리는 스케줄링
 	@Scheduled(cron = "30 * * * * *")
-	public void permitCheckInsert() {
-		log.info("스케줄실행");
-		TboardDTO dto = new TboardDTO();
-		dto.setCode("permit");
-		try {
-			bs.listPutBoard(dto);
-			
-			
-		} catch (Exception e) {
-			log.info("스케줄할 대상이 없음");
-		}
+	@Transactional
+	public void permitCheckAndComplete() {
+	    log.info("스케줄 실행");
+
+	    TboardDTO dto = new TboardDTO();
+	    dto.setCode("permit");
+
+	    try {
+	        // 첫 번째 작업 실행
+	        bs.listPutBoard(dto);
+	       
+	        log.info("첫 번째 작업 완료");
+
+	        // 두 번째 작업 실행 (50초 후)
+	        Thread.sleep(20000); // 20초 대기
+	        log.info("완료로 스케줄링");
+	        bs.permitCodeChangeComplete();
+	        // 두 번째 작업 로직 추가
+	        // bs.permitCodeToComplete(); // 예시로 추가
+
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	        log.info("스케줄할 대상이 없음");
+	    }
 	}
-	
-	//나중에 새볔시간에 변경하도록 위 스케줄링 보다 늦어야함
-	@Scheduled(cron = "50 * * * * *")
-	public void permitCodeToComplete() {
-		log.info("완료로 스케줄링");
-		try {
-			bs.permitCodeChangeComplete();
-			
-		} catch (Exception e) {
-			log.info("스케줄대상없음");
-		}
-	}
+
 }
--- IOgit/src/main/java/utils/ClobTypeHandler.java
+++ IOgit/src/main/java/utils/ClobTypeHandler.java
@@ -1,38 +1,30 @@
 package utils;
 
+import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.TypeHandler;
-
-import java.io.Reader;
 import java.sql.*;
 
-public class ClobTypeHandler implements TypeHandler<String> {
+public class ClobTypeHandler extends BaseTypeHandler<String> {
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
+		ps.setClob(i, new javax.sql.rowset.serial.SerialClob(parameter.toCharArray()));
+	}
 
-    @Override
-    public void setParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
-        if (parameter != null) {
-            Reader reader = new java.io.StringReader(parameter);
-            ps.setCharacterStream(i, reader, parameter.length());
-        } else {
-            ps.setNull(i, Types.CLOB);
-        }
-    }
+	@Override
+	public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		Clob clob = rs.getClob(columnName);
+		return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
+	}
 
-    @Override
-    public String getResult(ResultSet rs, String columnName) throws SQLException {
-        Clob clob = rs.getClob(columnName);
-        return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
-    }
+	@Override
+	public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		Clob clob = rs.getClob(columnIndex);
+		return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
+	}
 
-    @Override
-    public String getResult(ResultSet rs, int columnIndex) throws SQLException {
-        Clob clob = rs.getClob(columnIndex);
-        return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
-    }
-
-    @Override
-    public String getResult(CallableStatement cs, int columnIndex) throws SQLException {
-        Clob clob = cs.getClob(columnIndex);
-        return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
-    }
+	@Override
+	public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		Clob clob = cs.getClob(columnIndex);
+		return clob != null ? clob.getSubString(1, (int) clob.length()) : null;
+	}
 }
